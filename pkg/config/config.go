6e7bab6604d746542995be603d069964f3a4d9eb
Garvit <garvitgupta58@gmail.com>
2025-07-07 21:11:39 +0530

    30 workers

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -149,7 +149,7 @@ func defaultConfig() *Config {
 		GoBinaryEnvVars:  EnvList{"GOPROXY=direct"},
 		GoEnv:            "development",
 		GoGetWorkers:     10,
-		ProtocolWorkers:  100,
+		ProtocolWorkers:  30,
 		LogLevel:         "warn",
 		LogFormat:        "plain",
 		CloudRuntime:     "none",
